66ae882f5905687f5c00ca052cbbcb5f407ac2cc
hertelukas <luki.hertel@gmail.com>
2021-03-13 20:45:44 +0100

    Initial Commit

---
+++ src/main/java/com/werwolf/WerwolfApplication.java
@@ -0,0 +1,13 @@
+package com.werwolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerwolfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WerwolfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/werwolf/WerwolfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.werwolf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WerwolfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
