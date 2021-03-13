aec624c28fc6d45463eb5807de7192f304c9318b
Tuomas Pirhonen <typirhonen@icloud.com>
2021-03-14 00:51:03 +0100

    initialised stuff or sth

--- src/main/java/com/werwolf/game/GameController.java
+++ src/main/java/com/werwolf/game/GameController.java
@@ -1,4 +1,20 @@
 package com.werwolf.game;
 
 public class GameController {
+    boolean isActive;
+    boolean isNight;
+    Game game;
+    long wolfVoteTime;
+    long dayTime; // oder voteTime (muss noch evtl geklaert werden)
+
+    boolean nextDay() {
+        //TODO
+        return false;
+    }
+
+    boolean nextNight() {
+        //TODO
+        return false;
+    }
+
 }
