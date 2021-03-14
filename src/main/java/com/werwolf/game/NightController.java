9b3d185cdb0e58a196bb09b27a7e459c3096fec8
Tuomas Pirhonen <typirhonen@icloud.com>
2021-03-14 23:45:13 +0100

    paar comments, oPtiMizEd ImPorTS

--- src/main/java/com/werwolf/game/NightController.java
+++ src/main/java/com/werwolf/game/NightController.java
@@ -5,7 +5,6 @@ import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class NightController {
@@ -13,12 +12,12 @@ public class NightController {
     Game game;
     TextChannel channel;
     long voteTime;
-    List<Night> nights;
+    List<Night> nights; // Update am Anfang oder Ende der Nacht?
 
     public NightController(Game game, long wolfVoteTime) {
         this.game = game;
         voteTime = wolfVoteTime;
-        // channel = <jda oder so>.getTextChannelById(game.wolfChannelId);
+        // channel = <jda oder so>.getTextChannelById(game.wolfChannelId); how lol
     }
 
     void startNight() {
@@ -27,6 +26,7 @@ public class NightController {
         // mute all players in voice channel
         EmbedBuilder wolfVoteMsg = new EmbedBuilder();
         wolfVoteMsg.setTitle("who ded");
+        // scuffed but kinda works
         List<Player> alive = game.getPlayers().stream().filter(Player::isAlive).collect(Collectors.toList());
         for (int i = 0; i < alive.size(); i++)
             wolfVoteMsg.addField(alive.get(i).getUsername(), ":regional_indicator_" + (char) ('a' + i), false);
@@ -35,7 +35,7 @@ public class NightController {
 
 
         try {
-            TimeUnit.MILLISECONDS.sleep(voteTime);
+            TimeUnit.MILLISECONDS.sleep(voteTime); // this waits, but checks jackshit -> need to check if wolf voting ended
         } catch (InterruptedException ignored) {
         }
 
