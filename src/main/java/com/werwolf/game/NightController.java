57ccd3578a4ffa927909c2044e38f2034417b5c9
Tuomas Pirhonen <typirhonen@icloud.com>
2021-03-14 23:39:16 +0100

    bissl zueg

--- src/main/java/com/werwolf/game/NightController.java
+++ src/main/java/com/werwolf/game/NightController.java
@@ -1,11 +1,12 @@
 package com.werwolf.game;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class NightController {
 
@@ -25,13 +26,21 @@ public class NightController {
 
         // mute all players in voice channel
         EmbedBuilder wolfVoteMsg = new EmbedBuilder();
-        wolfVoteMsg.setTitle("Who should be killed?");
+        wolfVoteMsg.setTitle("who ded");
+        List<Player> alive = game.getPlayers().stream().filter(Player::isAlive).collect(Collectors.toList());
+        for (int i = 0; i < alive.size(); i++)
+            wolfVoteMsg.addField(alive.get(i).getUsername(), ":regional_indicator_" + (char) ('a' + i), false);
 
-        channel.sendMessage("").queue();
+        channel.sendMessage(wolfVoteMsg.build()).queue(msg -> msg.addReaction("platzhalter lol").queue()); // fix this
+
+
         try {
             TimeUnit.MILLISECONDS.sleep(voteTime);
         } catch (InterruptedException ignored) {
         }
+
+        // gather votes
+        // player.die()
 
         // unmute all players
 
