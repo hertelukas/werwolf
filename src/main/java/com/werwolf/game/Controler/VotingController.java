029c633fc8a0921471ce0e4f13dc5f6cebac7596
Julian <ge96xur@mytum.de>
2021-03-15 16:05:29 +0100

    exceptioncatch

---
+++ src/main/java/com/werwolf/game/Controler/VotingController.java
@@ -0,0 +1,12 @@
+package com.werwolf.game.Controler;
+
+import java.util.HashMap;
+
+public class VotingController {
+    private HashMap<Long, Integer> votings;
+
+
+    public VotingController(){
+
+    }
+}
