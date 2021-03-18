5b0936af914be4f9407ae51c036007bc1beaece1
Julian <ge96xur@mytum.de>
2021-03-18 20:55:12 +0100

    Serialkiller is working

--- src/main/java/com/werwolf/game/roles/Serialkiller.java
+++ src/main/java/com/werwolf/game/roles/Serialkiller.java
@@ -3,18 +3,30 @@ package com.werwolf.game.roles;
 import com.werwolf.game.Game;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 
 public class Serialkiller extends Player{
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(Serialkiller.class);
 
     public Serialkiller(Player player) {
         super(player.user, player.guild);
         this.characterType = CharacterType.SerialKiller;
     }
 
+    /**
+     * SerialKiller tötet den ausgewählten Spieler
+     * @param target Spieler der durch den serialkiller getötet wird
+     * @param votings Hashmap, die die Voting der Werewölfe beinhaltet (Falls später auch noch andere darauf zugriff haben sollen)
+     * @param game
+     */
     @Override
     public void vote(Player target, HashMap<Long, Integer> votings, Game game) {
         target.die(game);
+        game.getController().getNightController().getNights().peek().getDiedtonight().add(target);
+        LOGGER.info(getUsername() + "(SerialKiller) hat " + target.getUsername() + " umgebracht");
     }
 }
