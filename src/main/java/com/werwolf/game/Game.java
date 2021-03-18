2d83bfdc02388bda57ff37fd0877325e7d19c07c
hertelukas <luki.hertel@gmail.com>
2021-03-18 18:41:01 +0100

    Dead werewolves can't write in their channel

--- src/main/java/com/werwolf/game/Game.java
+++ src/main/java/com/werwolf/game/Game.java
@@ -259,7 +259,7 @@ public class Game {
             if(!player.getCharacterType().isCanSeeWWChannel()) continue;
             IPermissionHolder permissionHolder = new MemberImpl ((GuildImpl) guild, player.getUser());
             try {
-                if(value) Objects.requireNonNull(guild.getTextChannelById(wolfChannelID)).putPermissionOverride(permissionHolder).setAllow(Permission.MESSAGE_WRITE).queue();
+                if(player.isAlive() && value) Objects.requireNonNull(guild.getTextChannelById(wolfChannelID)).putPermissionOverride(permissionHolder).setAllow(Permission.MESSAGE_WRITE).queue();
                 else Objects.requireNonNull(guild.getTextChannelById(wolfChannelID)).putPermissionOverride(permissionHolder).setDeny(Permission.MESSAGE_WRITE).queue();
             }catch (Exception e){
                 LOGGER.warn("Failed to change writing permissions in werwolf channel: " + e.getMessage());
